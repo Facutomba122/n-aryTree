003a0ed18d4538a007cebd6d48567d3b78585a79
asdfasdasd qweqweqw <facundoelst@gmail.com>
2023-07-24 20:13:00 -0300

    Add insert operation for root node and leaf node

--- src/main/java/com/naryTreeDemo/nary/Dispatcher/NodesController.java
+++ src/main/java/com/naryTreeDemo/nary/Dispatcher/NodesController.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,12 +20,19 @@ public class NodesController {
     @Autowired
     private NodesService nodesService;
     
+    //GET ENDPOINTS
     @GetMapping("/nodes")
     public ArrayList<Nodes> viewAll(){
         ArrayList<Nodes> auxList = new ArrayList<>(nodesService.viewAll());
         return auxList;      
     }
     
+    @GetMapping("/nodes/{id}")
+    public Nodes findById(@PathVariable("id") UUID id){
+        return nodesService.findById(id);
+    }
+    
+    //POST ENDPOINTS
     @PostMapping("/nodes")
     public ResponseEntity<?> insertNewNode(@RequestParam("value")String value, @RequestParam(required = false) UUID parentId){
         return nodesService.insert(value, parentId);
--- src/main/java/com/naryTreeDemo/nary/Service/NodesService.java
+++ src/main/java/com/naryTreeDemo/nary/Service/NodesService.java
@@ -16,27 +16,29 @@ public class NodesService {
     @Autowired
     private NodesPersistence nodesPersistence;
     
-    private final Nodes parentNode = new Nodes();
+    private Nodes PARENTNODE = null;
     
     
     //Insert new Node nary tree
     public ResponseEntity<?> insert(String value, UUID parentId){
+        //Validation
         if (value == null){
             return new ResponseEntity("The value hasn't to be null", HttpStatus.BAD_REQUEST);
         }
-        if (parentNode.getId() == null){
-            nodesPersistence.save(parentNode);
+        if (nodesPersistence.findAll().isEmpty()){
+            PARENTNODE = new Nodes("parent", null, null);
+            nodesPersistence.save(PARENTNODE);
         }
+        
         Nodes newNode = new Nodes();
         newNode.setValue(value);
 
-
         //Without parent
         if (parentId == null){
-            newNode.setFatherNodeId(parentNode.getId());
+            newNode.setFatherNodeId(PARENTNODE.getId());
             nodesPersistence.save(newNode);
-            parentNode.addChildrenNode(newNode.getId());
-            nodesPersistence.save(parentNode);
+            PARENTNODE.addChildrenNode(newNode.getId());
+            nodesPersistence.save(PARENTNODE);
             return new ResponseEntity("Node create succesfully", HttpStatus.ACCEPTED);
         }
 
@@ -47,13 +49,30 @@ public class NodesService {
             newNode.setFatherNodeId(parentId);
             nodesPersistence.save(newNode);
             responseParentNode.get().addChildrenNode(newNode.getId());
+            nodesPersistence.save(responseParentNode.get());
             return new ResponseEntity("Node create succesfully", HttpStatus.ACCEPTED);
         } else {
             return new ResponseEntity("Parent node does not exists", HttpStatus.BAD_REQUEST);
         }
     }
     
+    
+    //CONSULT FUNCTIONS
     public List<Nodes> viewAll(){
         return nodesPersistence.findAll();
+    }
+    
+    public Nodes findById(UUID id){
+        try {
+            Optional<Nodes> responseNodes = nodesPersistence.findById(id);
+            if (responseNodes.isPresent()){
+                return responseNodes.get();
+            } else {
+                return new Nodes();
+            }
+        
+        } catch(Exception e) {
+            throw e; 
+        }
     }
 }
